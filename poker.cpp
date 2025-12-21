1b6b0475600161546e475d05c46ae21c0b4652e9
Ryan <mchldude@outlook.com>
2025-12-20 23:02:25 -0500

    cpucount and cpumoney

--- poker.cpp
+++ poker.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    cout << "gaur";
+    int money = 1000;
+    int cpu_count;
+    vector<int> cpu_money;
+
+    cout << "How many CPU players? ";
+    cin >> cpu_count;
+
+    for (int i = 0; i < cpu_count; i++) {
+        cpu_money.push_back(1000);
+    }
+
+    for (int i = 0; i < cpu_money.size(); i++) {
+            cout << cpu_money[i] << " ";
+    }
+
     return 0;
 }

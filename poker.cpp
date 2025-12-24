a2d480402079869b317867a7db223ad3c5848555
Ryan <mchldude@outlook.com>
2025-12-23 22:02:44 -0500

    cleaned up the code by using enums and structs for cards

--- poker.cpp
+++ poker.cpp
@@ -8,28 +8,38 @@
 using namespace std;
 
 int main() {
-    //get random seed
+    // random seed
     random_device rd;
     mt19937 rng(rd());
 
-    // holds the money for the player and the cpu players
     int money = 1000;
     int cpu_count;
     vector<int> cpu_money;
 
-    vector<string> card_dictionary = {"1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "11h", "12h", "13h", 
-        "1d", "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "10d", "11d", "12d", "13d", 
-        "1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", "11s", "12s", "13s", 
-        "1c", "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "10c", "11c", "12c", "13c"};
-    
-    // holds and loads the cards for the player and cpu players
-    vector<string> deck = card_dictionary;
-    shuffle(deck.begin(), deck.end(), rng);
-    vector<string> player_cards;
-    vector<string> load_cards;
-    vector<vector<string> > cpu_cards;
+    enum Suit { hearts, diamonds, clubs, spades };
+    enum Value { two = 2, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace };
 
-    // start a game of poker
+    struct Card {
+        Suit suit;
+        Value value;
+    };
+
+    vector<Card> deck;
+    vector<Card> player_cards;
+    vector<Card> load_cards;
+    vector<vector<Card> > cpu_cards;
+
+    // creates and adds every card to the list of cards
+    for (int s = hearts; s < spades; s++) {
+        for (int v = two; v < ace; v++) {
+            Card new_card;
+            new_card.value = static_cast<Value>(v + 1);
+            new_card.suit = static_cast<Suit>(s + 1);
+            deck.push_back(new_card);
+        }
+    }
+    shuffle(deck.begin(), deck.end(), rng);
+
     string start_game;
     cout << "Would you like to start a game of poker?(yes/no) ";
     cin >> start_game;
@@ -42,24 +52,28 @@ int main() {
             cpu_money.push_back(1000);
         }
 
+        // prints out the cpu_money for debugging
         for (int i = 0; i < cpu_money.size(); i++) {
                 cout << cpu_money[i] << " ";
         }
 
-        // deals the player's cards
+        // deals the cards
         for (int i = 0; i < 2; i++) { player_cards.push_back(deck.back()); deck.pop_back(); }
 
-        // deals the cpus' cards
         for (int i = 0; i < cpu_count; i++) {
             for (int i = 0; i < 2; i++) { load_cards.push_back(deck.back()); deck.pop_back(); }
             cpu_cards.push_back(load_cards);
 
-            // clears the load_cards vector
             load_cards.clear();
         }
+
+        // prints out the cpus' cards for debugging and stuff
         for (int i = 0; i < cpu_cards.size(); i++) {
-            cout << cpu_cards[i][0] << " ";
-            cout << cpu_cards[i][1] << " ";
+                cout << "[1st card value: " << static_cast<int>(cpu_cards[i][0].value) << ", ";
+                cout << "1st card suit: " << static_cast<int>(cpu_cards[i][0].suit) << "], ";
+                cout << "[2nd card value: " << static_cast<int>(cpu_cards[i][1].value) << ", ";
+                cout << "2nd card suit: " << static_cast<int>(cpu_cards[i][1].suit) << "]], ";
+
         }
     }
 
